53f30e2edd8d9a222a5dcb0a9289324d11110d82
Clement ORMAUX <clement.ormaux@gmail.com>
2019-05-31 11:37:06 +0200

    new commit Etape7

---
+++ src/main/java/App.java
@@ -0,0 +1,8 @@
+import com.github.lalyos.jfiglet.FigletFont;
+
+public class App {
+  public static void main(String[] args) {
+    String asciiArt = FigletFont.convertOneLine("hello");
+    System.out.println(asciiArt);
+  }
+}
